f2844c14359dd6e5d427d8f9c718f277bc1b1d36
BzzzThe18th <69125495+BzzzThe18th@users.noreply.github.com>
2023-08-09 07:44:34 -0700

    Add files via upload

---
+++ GOOMPSCollision.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace GOOMPS
+{
+    class GOOMPSCollision : MonoBehaviour
+    {
+        public static void ChangePlayerVisibility(VRRig _rig, bool _visible)
+        {
+            if (Cfg.audio.Value) GorillaTagger.Instance?.offlineVRRig?.tagSound.PlayOneShot(GorillaTagger.Instance.offlineVRRig.clipToPlay[5]);
+            _rig.mainSkin.enabled = _visible;
+            _rig.muted = Cfg.mute.Value ? !_visible : false;
+            _rig.transform.Find("rig/body/gorillachest").gameObject.SetActive(_visible);
+            _rig.transform.Find("rig/body/head/gorillaface").gameObject.SetActive(_visible);
+        }
+
+        void OnTriggerEnter(Collider coll)
+        {
+            if (coll.name.Contains("Body"))
+            {
+                ChangePlayerVisibility(coll.transform.parent.parent.parent.GetComponent<VRRig>(), false);
+            }
+        }
+
+        void OnTriggerExit(Collider coll)
+        {
+            if (coll.name.Contains("Body"))
+            {
+                ChangePlayerVisibility(coll.transform.parent.parent.parent.GetComponent<VRRig>(), true);
+            }
+        }
+    }
+}
